d5c0aac64d8904641df8318e464990ee8b6665e7
Markus <condrenmarkus@gmail.com>
2022-04-26 21:00:24 +0100

    Update

---
+++ bingespark/partials/navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-default" id="navbar">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                    <a class="navbar-brand" href="index.php"><img src="images/bingesparkLogo.png" alt="bingespark logo" id="bingespark-logo"></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                    <div class="col-sm-6">
+                        <form class="navbar-form navbar-right">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                    </div>
+
+                    <ul class="nav navbar-nav">
+
+                        <li role="separator" class="divider"></li>
+                        <li><a href="explore.php">Explore</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="contact.php">Contact</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="random.php">Random</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="profile.php" id="profile-btn">Profile<span class="sr-only">(current)</span></a></li>
+
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
