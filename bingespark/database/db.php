ba1653b72f1ba23a4187b9c416f3c04169c24b7f
Markus <condrenmarkus@gmail.com>
2022-04-27 12:13:29 +0100

    Update

--- bingespark/database/db.php
+++ bingespark/database/db.php
@@ -1,21 +1,46 @@
 <?php
- 
- $host = "localhost";
- $user = "root";
- //$pw = ""; //MAMP
- $pw = "root"; //XAMPP
- $db = "webdev";
- 
- $conn = new mysqli($host, $user, $pw, $db);
- 
- if ($conn->connect_error) {
- 
-      $check = "not connected ".$conn->connect_error;
- 
-        }else{
- 
-     $check="Connected to your mysql DB.";
- 
- }
- 
+
+$host = "localhost";
+$user = "root";
+$pw = ""; //MAMP
+//$pw = "root"; //XAMPP
+$db = "bingespark_test";
+
+$conn = new mysqli($host, $user, $pw, $db);
+
+if ($conn->connect_error) {
+
+  $check = "not connected " . $conn->connect_error;
+} else {
+
+  $check = "Connected to your mysql DB.";
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  include("partials/head.php")
+  ?>
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="title">
+      <?php
+      echo $check;
+      ?>
+    </h1>
+
+    <p class="subtitle">
+
+    </p>
+
+  </div>
+</body>
+
+</html>
--- bingespark/profile.php
+++ bingespark/profile.php
@@ -67,11 +67,19 @@
 
                 <div class="row">
                     <div class="col-xs-4 col-sm-4 col-md-4"><img src="#" alt="profile picture"></div>
-                    <div class="col-xs-8 col-sm-8 col-md-8">
+                    <div class="col-xs-4 col-sm-4 col-md-4">
                         <ul>
-                            <li>First Name Last Name</li>
+
+                            <li>Full Name</li>
+                     
+                        </ul>
+
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <ul>
+
                             <li>username (email)</li>
-                            <li role="separator" class="divider"></li>
+                     
                         </ul>
 
                     </div>
@@ -108,12 +116,31 @@
                             <!---Change to dynamic username?-->
                         </div>
                         <nav id="profile-pills">
-                            <ul class="nav nav-pills nav-stacked">
+                            <ul class="nav nav-pills nav-stacked navbar-">
+                                <!-- Make active pill #FF4000-->
                                 <li role="presentation" class="active"><a href="#">Favourites</a></li>
                                 <li role="presentation"><a href="#">Reviews</a></li>
                                 <li role="presentation"><a href="#">Settings</a></li>
                             </ul>
                         </nav>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <div class="d-flex align-items-start">
+                            <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                                <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+                                <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+                                <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+                                <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+                            </div>
+                            <div class="tab-content" id="v-pills-tabContent">
+                                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+                                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
